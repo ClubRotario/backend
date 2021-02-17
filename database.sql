ec962ea6622ded0ee87be9179ddc5092d5328e87
Angel Castillo <portillocastilloa@gmail.com>
2021-02-16 21:23:44 -0600

    Script de la base de datos actualizado 16022021

--- database.sql
+++ database.sql
@@ -3,91 +3,90 @@
 CREATE DATABASE rotary_club;
 USE rotary_club;
 
-CREATE TABLE roles(
-    role_id TINYINT NOT NULL,
-    role_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY(role_id)
-)ENGINE=InnoDB;
-
-CREATE TABLE tags(
-    tag_id INT NOT NULL,
-    tag_name VARCHAR(30) NOT NULL,
-    tag_description VARCHAR(100) NOT NULL,
-    PRIMARY KEY(tag_id)
-)ENGINE=InnoDB;
-
-CREATE TABLE categories(
-    category_id INT AUTO_INCREMENT NOT NULL,
-    category_name VARCHAR(70) NOT NULL,
-    PRIMARY KEY(category_id)
-)ENGINE=InnoDB;
-
-
 CREATE TABLE users(
-    user_id INT AUTO_INCREMENT NOT NULL,
-    role_id TINYINT NOT NULL,
-    firts_name VARCHAR(70) NOT NULL,
-    last_name VARCHAR(70) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(255) NOT NULL,
+    user_id INT AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(70) UNIQUE NOT NULL,
     phone VARCHAR(11) NOT NULL,
-    country VARCHAR(20) NOT NULL,
-    address VARCHAR(70 NOT NULL),
+    address VARCHAR(70) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     created_at DATETIME DEFAULT NOW(),
     updated_at DATETIME DEFAULT NOW(),
     last_login DATETIME DEFAULT NOW(),
-    PRIMARY KEY(user_id),
-    FOREIGN KEY(role_id) REFERENCES roles(role_id)
-)ENGINE=InnoDB;
-
+    role_id TINYINT NOT NULL,
+    active TINYINT(1) DEFAULT 1,
+    PRIMARY KEY(user_id)
+);
 
 CREATE TABLE posts(
-    post_id INT AUTO_INCREMENT NOT NULL,
+    post_id INT AUTO_INCREMENT,
     user_id INT NOT NULL,
-    category_id INT NOT NULL,
-    title VARCHAR(255) NOT NULL,
-    content text COLLATE utf8_unicode_ci NOT NULL,
-    updated_at DATETIME DEFAULT NOW(),
+    category_id TINYINT NOT NULL,
+    title VARCHAR(100) NOT NULL,
     published_at DATETIME DEFAULT NOW(),
+    updated_at DATETIME DEFAULT NOW(),
+    content TEXT NULL,
+    description VARCHAR(170) NOT NULL,
     published TINYINT(1) DEFAULT 0,
-    PRIMARY KEY(post_id),
-    FOREIGN KEY(user_id) REFERENCES users(user_id),
-    FOREIGN KEY(category_id) REFERENCES categories(category_id)
-)ENGINE=InnoDB;
+    PRIMARY KEY(post_id)
+);
 
-CREATE TABLE agendas(
+CREATE TABLE entries(
     post_id INT NOT NULL,
-    PRIMARY KEY(post_id),
-    FOREIGN KEY(post_id) REFERENCES posts(post_id)
-)ENGINE=InnoDB;
+    entry_date DATETIME DEFAULT NOW(),
+    PRIMARY KEY(post_id)
+);
 
 CREATE TABLE images(
-    image_id INT AUTO_INCREMENT NOT NULL,
-    post_id INT NOT NULL,
+    image_id INT AUTO_INCREMENT,
     name VARCHAR(255) NOT NULL,
-    upload_at DATETIME DEFAULT NOW(),
-    PRIMARY KEY(image_id),
-    FOREIGN KEY(post_id) REFERENCES posts(post_id)
-)ENGINE=InnoDB;
-
-CREATE TABLE posts_tags(
     post_id INT NOT NULL,
-    tag_id INT NOT NULL,
-    PRIMARY KEY(post_id, tag_id),
-    FOREIGN KEY(post_id) REFERENCES posts(post_id),
-    FOREIGN KEY (tag_id) REFERENCES tags(tag_id)
-)ENGINE=InnoDB;
+    upload_at DATETIME DEFAULT NOW(),
+    PRIMARY KEY(image_id)
+);
 
 CREATE TABLE codes(
     code_id SMALLINT AUTO_INCREMENT,
     user_id INT NOT NULL,
-    code VARCHAR(6),
+    code VARCHAR(6) NOT NULL,
     PRIMARY KEY(code_id)
-)ENGINE=InnoDB;
+);
 
+CREATE TABLE roles(
+    role_id TINYINT AUTO_INCREMENT,
+    role VARCHAR(30) NOT NULL,
+    PRIMARY KEY(role_id)
+);
+
+CREATE TABLE categories(
+    category_id TINYINT AUTO_INCREMENT,
+    category VARCHAR(20) NOT NULL,
+    PRIMARY KEY(category_id)
+);
+
+CREATE TABLE posts_tags(
+    tag_id SMALLINT NOT NULL,
+    post_id INT NOT NULL,
+    PRIMARY KEY(tag_id, post_id)
+);
+
+CREATE TABLE tags(
+    tag_id SMALLINT AUTO_INCREMENT,
+    tag_content VARCHAR(20) NOT NULL,
+    tag_description VARCHAR(100) NOT NULL,
+    PRIMARY KEY(tag_id)
+);
 
 ALTER TABLE codes ADD FOREIGN KEY(user_id) REFERENCES users(user_id);
-ALTER TABLE users ADD COLUMN active TINYINT(1) DEFAULT 1;
+ALTER TABLE users ADD FOREIGN KEY(role_id) REFERENCES roles(role_id);
+ALTER TABLE posts ADD FOREIGN KEY(user_id) REFERENCES users(user_id);
+ALTER TABLE posts ADD FOREIGN KEY(category_id) REFERENCES categories(category_id);
+ALTER TABLE images ADD FOREIGN KEY(post_id) REFERENCES posts(post_id);
+ALTER TABLE posts_tags ADD FOREIGN KEY(tag_id) REFERENCES tags(tag_id);
+ALTER TABLE posts_tags ADD FOREIGN KEY(post_id) REFERENCES posts(post_id);
+ALTER TABLE entries ADD FOREIGN KEY(post_id) REFERENCES posts(post_id);
+
 
 
 -- Seeders de la base de datos para poblarla
