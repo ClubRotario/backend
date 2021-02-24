2292a93a73befc3656ff428449e1b6b8690d0259
unknown <anavillmar98@gmail.com>
2021-02-23 23:41:18 -0600

    primeros pasos de las pages por el lado del user

--- src/controllers/web.controller.js
+++ src/controllers/web.controller.js
@@ -3,8 +3,11 @@ const  { request, response } = require('express');
 const pool = require('../config/database');
 
 const indexController = async(req = request, res = response) => {
-    const [post] = await pool.query("SELECT * from posts WHERE post_id=12");
+    const [post] = await pool.query("SELECT * from posts WHERE post_id=1");
     res.render('pages/index', { title: "Inicio", post });
 };
 
-module.exports = { indexController };
+const aboutusController = (req = request, res = response) => {
+    return res.render('pages/aboutus', {title:'Acerca de Nosotros'} );
+}
+module.exports = { indexController, aboutusController };
--- src/routes/web.routes.js
+++ src/routes/web.routes.js
@@ -3,8 +3,9 @@ const router = express.Router();
 
 //Importacion de controladores 
 //Este apartado sera el accedido por los usuarios, esta separado de las rutas de la API
-const { indexController } = require('../controllers/web.controller');
+const { indexController, aboutusController } = require('../controllers/web.controller');
 
 router.get( '/', indexController );
+router.get( '/nosotros', aboutusController);
 
 module.exports = router;
